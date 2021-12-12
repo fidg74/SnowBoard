9ab4206e34a4927bdbf77701f794823601d51cdb
lipliepra <dzis99@yandex.ru>
2021-12-12 16:23:29 +0500

    profile

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ require('../css/app.css');
 require('../scss/style.scss');
 require('../scss/home.scss');
 require('../scss/catalog.scss');
+require('../scss/profile.scss');
+require('../scss/busket.scss');
 
 /********** Paste your code here! ************/
 
