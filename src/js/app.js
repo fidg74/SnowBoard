16122a1825d6e5625673c422f9c742137ab20aeb
lipliepra <dzis99@yandex.ru>
2021-12-12 15:23:04 +0500

    catalog

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ const testModules = require('./test-module');
 require('../css/app.css');
 require('../scss/style.scss');
 require('../scss/home.scss');
+require('../scss/catalog.scss');
 
 /********** Paste your code here! ************/
 
